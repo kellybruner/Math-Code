49cdda8d5c674c04f0c114974c4cf2f9c4eca151
kellybruner <brunerka@dukes.jmu.edu>
2016-04-22 17:50:25 -0400

    Create math.js

---
+++ math.js
@@ -0,0 +1,19 @@
+// export a function for doing addition
+exports.add = function(a, b) {
+	return a + b;
+};
+
+// export a function for doing subtraction
+exports.subtract = function(a, b) {
+	return a - b;
+};
+
+// export a function for doing multiplication
+exports.multipy = function(a, b) {
+	return a * b;
+};
+
+// export a function for doing division
+exports.divide = function(a, b) {
+	return a / b;
+};
